1bd861138d93f20c75f7ec205810fe18918e804e
Naoki Takahashi <naoki.takahashi060@gmail.com>
2023-11-18 08:58:16 +0900

    Add bringup route server

--- nav_waypoint_bringup/launch/waypoint_manager.launch.py
+++ nav_waypoint_bringup/launch/waypoint_manager.launch.py
@@ -16,6 +16,9 @@ def generate_launch_description():
     waypoints_file_param = {
         'waypoints_file': launch.substitutions.LaunchConfiguration('waypoints_file')
     }
+    route_file_param = {
+        'route_file': launch.substitutions.LaunchConfiguration('route_file')
+    }
     use_sim_time_param = {
         'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')
     }
@@ -33,18 +36,42 @@ def generate_launch_description():
             )
         ),
         launch.actions.DeclareLaunchArgument(
+            'route_file',
+            default_value=os.path.join(
+                this_pkg_share_dir,
+                'config',
+                'route.yaml'
+            )
+        ),
+        launch.actions.DeclareLaunchArgument(
             'use_sim_time',
             default_value='false'
         ),
         launch.actions.GroupAction([
+            launch.actions.TimerAction(
+                period=1.0,
+                actions=[
+                    launch_ros.actions.Node(
+                        package='nav_waypoint_server',
+                        executable='nav_waypoint_server_node',
+                        name='nav_waypoint_server',
+                        output='screen',
+                        parameters=[
+                            waypoint_frame_id_param,
+                            waypoints_file_param,
+                            use_sim_time_param
+                        ]
+                    )
+                ]
+            ),
             launch_ros.actions.Node(
                 package='nav_waypoint_server',
-                executable='nav_waypoint_server_node',
-                name='nav_waypoint_server',
+                executable='nav_route_server_node',
+                name='nav_route_server',
                 output='screen',
                 parameters=[
                     waypoint_frame_id_param,
-                    waypoints_file_param,
+                    route_file_param,
                     use_sim_time_param
                 ]
             ),
@@ -52,12 +79,26 @@ def generate_launch_description():
                 package='nav_waypoint_conversion',
                 executable='pose_stamped_to_nav_waypoint_node',
                 name='pose_stamped_to_nav_waypoint',
+                output='screen',
                 parameters=[
                     use_sim_time_param
                 ],
                 remappings=[
-                    ('~/input_pose', '/goal_pose'),
+                    ('~/input_pose', '/set_waypoint'),
                     ('~/output_waypoint', 'nav_waypoint_server/regist')
+                ]
+            ),
+            launch_ros.actions.Node(
+                package='nav_waypoint_conversion',
+                executable='nav_waypoint_to_pose_stamped_node',
+                name='nav_waypoint_to_pose_stamped',
+                output='screen',
+                parameters=[
+                    use_sim_time_param
+                ],
+                remappings=[
+                    ('~/input_waypoint', 'nav_route_server/waypoint'),
+                    ('~/output_pose', '/goal_pose')
                 ]
             )
         ])
